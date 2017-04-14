7daaafcffb09d81302ff8fa45272f7540f81e67b
Wiinterfell <slegras.contact@gmail.com>
2017-04-14 18:41:47 +0100

    Fix build

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def log(message):  # simple wrapper for logging to stdout on heroku
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', , port=483)
+    app.run(debug=True, host='0.0.0.0', port=483)
